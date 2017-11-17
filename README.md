90f50823b157fcb244cf3c8bd4c6fc95f2afb1b7
Larry Mckuydee <larry.mckuydee@gmail.com>
2017-11-17 11:21:15 +0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# opencart_setup
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: opencart_setup
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
